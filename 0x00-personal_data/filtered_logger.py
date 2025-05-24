5e1d41591eba68fa5f3ba83e3f80b911fdc64249
Alfred Tuva <muindetuva@gmail.com>
2025-05-24 17:41:35 +0300

    Add task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@ Module for filtering sensitive data from log messages.
 """
 
 import re
-from typing import List
+import logging
+from typing import List, Tuple
 
 
 def filter_datum(
@@ -18,3 +19,23 @@ def filter_datum(
         lambda m: f"{m.group(1)}={redaction}{separator}",
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Tuple[str]):
+        """Initialize the formatter with fields to redact."""
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format and redact sensitive fields in log messages."""
+        original = record.getMessage()
+        redacted = filter_datum(self.fields, self.REDACTION, original, self.SEPARATOR)
+        record.msg = redacted
+        return super().format(record)
