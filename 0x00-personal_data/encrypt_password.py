9f0f2742c97a1d57289f48b44a62b96d66cfb78b
Alfred Tuva <muindetuva@gmail.com>
2025-05-27 17:34:53 +0300

    Add task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,10 @@ def hash_password(password: str) -> bytes:
     Hash a password using bcrypt with automatic salting.
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Verify a password against a given hashed password.
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
