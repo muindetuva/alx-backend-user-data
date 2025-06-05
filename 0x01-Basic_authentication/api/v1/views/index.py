fdc549a0f6e98206c23b143257e06e94d9a22360
Alfred Tuva <muindetuva@gmail.com>
2025-06-05 12:38:34 +0300

    Add task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> str:
     Raisesa a 401 error to test the error handler
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Raises a 403 error to test the error handler.
+    """
+    abort(403)
